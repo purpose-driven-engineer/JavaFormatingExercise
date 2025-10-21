bd68a8ad7f497e2d076373a4d2bfb5f1c7345ea1
Ola Popoola <7555474+purpose-driven-engineer@users.noreply.github.com>
2025-10-21 18:05:26 +0100

    completed number format exercise

---
+++ src/CastingExercise.java
@@ -0,0 +1,38 @@
+public class CastingExercise {
+    public static void main(String[] args){
+
+        /*
+            2 types of casting
+            1. Widening (Automatic) Casting - cast from small datatype to large datatype e.g. byte -> short, etc.
+            2. Narrowing (Manual) Casting - cast from large to small datatype - requires to be explicit else causes data loss
+
+         */
+
+        int intData = 85;
+        double doubleData = intData; // automatic casting
+        System.out.println("Int: " + intData);
+        System.out.println("Double: " + doubleData);
+
+        // manual casting
+        double price = 99.99;
+        int dollars = (int) price; // data loss - decimal part
+        System.out.println("Price: " + price);
+        System.out.println("Dollars: " + dollars);
+
+        // data loss - overflow
+        long bigNumber = 10000000000000L;
+        int smallNumber = (int) bigNumber;
+        System.out.println("Long: " + bigNumber);
+        System.out.println("Small: " + smallNumber); // wrong output
+
+        // using casting calculations
+        int a = 5;
+        int b = 2;
+        double result1 = a / b;
+        double result2 = (double) a / b;
+        System.out.println("Casting: "+ result1);
+        System.out.println("No Casting: "+ result2);
+
+
+    }
+}
